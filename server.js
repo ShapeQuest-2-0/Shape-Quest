a249d55c6b49ae74982a1515c08e40a6e58fea66
AVA <ava@pop-os.localdomain>
2023-10-21 12:13:43 -0500

    setup server and login/signup routes

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const apiRoutes = require('./routes/api');
+const userController = require('./routes/userController');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+
 mongoose.connect('mongodb+srv://shapequestuser:shapequest@cluster0.iecn0o7.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-
 db.on('error', (err) => {
     console.log('Error connecting to db')
 })
@@ -18,14 +20,27 @@ db.once('open', () => {
 console.log('Connected to Database');
 });
 
+const userSchema = new mongoose.Schema({
+    userName: {type: String, requires: true},
+    password: {type: String, require: true},
+    progression: {type: Number}
+})
+const User = mongoose.model('User', userSchema)
+
+
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
-res.send(200)
+    res.send(200)
 })
 
-app.post('/', (req, res) => {
+app.use('/', apiRoutes)
 
+//unknown route handler
+app.use((req, res) => {
+    console.log('Invalid page')
+    res.status(400)
 })
 
 
