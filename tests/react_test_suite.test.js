5f285a13b0d91db6de8eecd2e678d75b57277a44
Andrew Lo <andrewlo@Carolines-iMac.lan>
2023-10-24 22:06:54 -0700

    need to revert to previous commit

--- tests/react_test_suite.test.js
+++ tests/react_test_suite.test.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
+import { findAllByRole, render } from '@testing-library/react';
+/**
+ * @jest-environment jsdom
+ */
+import App from '../src/app';
 import LoginForm from '../src/components/LoginForm';
 
+// const jsdom = require('jsdom')
+// const { JSDOM } = jsdom;
 //does the login form render input fields?..
+//test the buttons
 
 describe('Unit testing React components', () => {
-  
+
+  describe('LoginForm', () => {
+    const props = {
+      username: 'Robert',
+      password: 'stabrabbit',
+      loggedIn: false
+    }
+
+
+    // beforeAll(() => {
+    //   loginForm = render(<LoginForm />)
+    // });
+
+
+    // test('Contains a login button', async () => {
+    //   render(<LoginForm {...props} />)
+    //   const buttons = await findAllByRole('button');
+    //   console.log(buttons);
+    //   expect(buttons.length).toBe(1);
+    // })
+
+  })
 })
