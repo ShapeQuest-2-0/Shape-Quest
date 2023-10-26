2fc8530ea24f5898bf9586d25f2cfbddb2ca22ab
lomeintheory <andrewlo957@gmail.com>
2023-10-25 22:56:10 -0700

    colortools and shapetools testing built out

--- tests/react_test_suite.test.js
+++ tests/react_test_suite.test.js
@@ -1,93 +1,115 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, getByText, waitFor, toHaveValue, toHaveBeenCalledWith, toHaveBeenCalledTimes } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import userEvent from '@testing-library/user-event';
+import { userEvent, user } from '@testing-library/user-event';
 
 import LoginForm from '../src/components/LoginForm';
 import ColorTools from '../src/components/ColorTools';
-=======
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'
+import ShapeTools from '../src/components/ShapeTools';
 
-import LoginForm from '../src/components/LoginForm';
-
->>>>>>> dev
-//does the login form render input fields?..
-//test the buttons
 
 describe('Unit testing React components', () => {
 
   describe('LoginForm', () => {
-<<<<<<< HEAD
-
-=======
-    let loginForm;
->>>>>>> dev
     const props = {
       username: 'Robert',
       password: 'stabrabbit',
-      loggedIn: false
     };
+    const setUsername = jest.fn();
+    const setPassword = jest.fn();
+    const onSubmit = jest.fn();
 
-<<<<<<< HEAD
     beforeEach(() => {
-=======
-    beforeAll(() => {
->>>>>>> dev
       render(
         <Router>
           <Routes>
-            <Route path="/" element={<LoginForm {...props}/>} />
+            <Route path="/" element={<LoginForm {...props} onSubmit={onSubmit} setUsername={setUsername}/>} />
           </Routes>
         </Router>
         )
-<<<<<<< HEAD
     });
-=======
-    })
->>>>>>> dev
 
     test('Contains a Login In button', async () => {
-      
       const buttons = await screen.findAllByRole('button');
       // console.log('this should be the amount of buttons: ', buttons.length)
       // console.log('login button: ', buttons)
       expect(buttons.length).toBe(1);
       expect(buttons[0]).toHaveTextContent('Log In');
-
     })
 
-<<<<<<< HEAD
-    test('Login button does not trigger when clicked without username and password input', async () => {
-      const props = {
-        handleSubmit: jest.fn(),
-      }
+    // test('Username and password input fields render when text is typed in', async () => {
+    //   const userNameInput = screen.getByPlaceholderText('Write Username');
+    //   await userEvent.type(userNameInput, 'username');
+    //   await waitFor(() => {
+    //     // expect(userNameInput).toHaveValue('username'); 
+    //     expect(setUsername).toHaveBeenCalledWith('username');
+    //   })
+    //   // console.log('should be username input field: ', userNameInput);
 
-      const buttons = await screen.findAllByRole('button');
-      console.log(buttons[0]);
-      userEvent.click(buttons[0]);
-      expect(props.handleSubmit).toHaveBeenCalledTimes(0);
-    })
+    // })
 
-  })
-
-  describe('Unit testing ColorTools', () => {
-    
-    describe('ColorTools', () => {
-      beforeEach(() => {
-        render(<ColorTools />)
+    test('Login button triggers submit when input fields have text', async () => {
+      const loginButton = screen.getByText('Log In');
+      const userNameInput = screen.getByPlaceholderText('Write Username');
+      const passwordInput = screen.getByPlaceholderText('Write Password');
+      userEvent.type(userNameInput, 'username');
+      userEvent.type(passwordInput, 'password');
+      userEvent.click(loginButton);
+      await waitFor(() => {
+        expect(onSubmit).toHaveBeenCalledTimes(1);
       })
     })
-  })
+    // test('Login button does not trigger when clicked without username and password input', async () => {
+    //   const props = {
+    //     username: 'Robert',
+    //     password: 'stabrabbit',
+    //     onSubmit: jest.fn()
+    //   }
+    //   // test is currently passing because props username and password do not seem to be rendering as input field values
 
-=======
-    test('HandleSubmit function is invoked when button is clicked', async () => {
-      const buttons = await screen.findAllByRole('button');
-      
+    //   const button = screen.getByText('Log In');
+    //   const usernameInput = screen.getAllByRole('textbox')[0];
+    //   // const passwordInput = screen.getAllByRole('textbox');
+    //   console.log('username input: ', usernameInput);
+    //   // console.log('password input: ', passwordInput);
+    //   userEvent.type(usernameInput, props.username);
+    //   expect(usernameInput).toHaveValue(props.username);
+    //   // console.log('this is our login button', button)
+    //   // await waitFor(() => userEvent.click(button));
+    //   // expect(props.onSubmit).toHaveBeenCalledTimes(0);
     })
 
   })
->>>>>>> dev
+
+  
+    
+    describe('ColorTools', () => {
+
+      test('Renders the specified color when clicked', async () => {
+        const setSelectedColor = jest.fn();
+        render(<ColorTools setSelectedColor={setSelectedColor}/>)
+        const redButton = screen.getByText('Red');
+        // console.log('this should be the redButton: ', screen.getByText('Red'))
+        userEvent.click(redButton);
+        await waitFor(() => {
+          expect(setSelectedColor).toHaveBeenCalledWith('red'); // this will pass the func has red passed in, will fail if blue/green/anything else is passed in
+        })
+      })
+
+    describe('ShapeTools', () => {
+
+      test('Renders the specified shape when clicked', async () => {
+        const setSelectedShape = jest.fn();
+        render(<ShapeTools setSelectedShape={setSelectedShape}/>)
+        const circleButton = screen.getByText('Circle');
+        userEvent.click(circleButton);
+        await waitFor(() => {
+          expect(setSelectedShape).toHaveBeenCalledWith('circle');
+        })
+      })
+    })
+      
+  // })
+
 })
